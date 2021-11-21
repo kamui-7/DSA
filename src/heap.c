938aa932832856eb0ee15401da82b53200b91c2c
Kamui <fin-kamui@pm.me>
2021-11-21 16:04:05 -0600

    feat: heapsort

--- src/heap.c
+++ src/heap.c
@@ -2,8 +2,25 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
+#include "math.h"
+
+int get_size(struct MaxHeap *heap) { return heap->size; }
+
+bool heap_is_empty(struct MaxHeap *heap) { return heap->size == 0; }
 
 int heap_get_max(struct MaxHeap *heap) { return heap->data[0]; }
+
+int extract_max(struct MaxHeap *heap) {
+    int max_item = heap_get_max(heap);
+    heap_remove(heap, 0);
+    return max_item;
+}
+
+void swap_arr(int arr[], int indx, int parent_indx) {
+    int temp = arr[parent_indx];
+    arr[parent_indx] = arr[indx];
+    arr[indx] = temp;
+}
 
 void heap_insert(struct MaxHeap *heap, int key) {
     if (heap->size == heap->capacity) {
@@ -35,12 +52,62 @@ void insert_rec(struct MaxHeap *heap, int indx) {
         return;
     }
 
-    sift_up(heap, indx, parent);
+    swap_arr(heap->data, indx, parent);
     insert_rec(heap, parent);
 }
 
-void sift_up(struct MaxHeap *heap, int indx, int parent_indx) {
-    int temp = heap->data[parent_indx];
-    heap->data[parent_indx] = heap->data[indx];
-    heap->data[indx] = temp;
+void heap_remove(struct MaxHeap *heap, int indx) {
+    swap_arr(heap->data, heap->size - 1, indx);
+    heap->data[heap->size - 1] = 0;
+    heap->size--;
+    remove_rec(heap, indx);
+}
+
+void remove_rec(struct MaxHeap *heap, int indx) {
+    int child_left = 2 * indx + 1;
+    int child_right = 2 * indx + 2;
+
+    if (heap->data[indx] >= heap->data[child_left] &&
+        heap->data[indx] >= heap->data[child_right]) {
+        return;
+    }
+
+    int larger_child = MIN(child_left, child_right);
+
+    swap_arr(heap->data, larger_child, indx);
+    remove_rec(heap, larger_child);
+}
+
+void heapify(int arr[], int indx, int size) {
+    int child_left = 2 * indx + 1;
+    int child_right = 2 * indx + 2;
+
+    if (child_left >= size || child_right >= size) {
+        return;
+    }
+
+    if (arr[indx] >= arr[child_left] && arr[indx] >= arr[child_right]) {
+        return;
+    }
+
+    int larger_child;
+
+    if (arr[child_left] > arr[child_right]) {
+        larger_child = child_left;
+    } else {
+        larger_child = child_right;
+    }
+
+    swap_arr(arr, larger_child, indx);
+    heapify(arr, larger_child, size);
+}
+
+void heapsort(int arr[], int size) {
+    int csize = size;
+
+    while (csize > 0) {
+        heapify(arr, 0, csize);
+        swap_arr(arr, 0, csize - 1);
+        csize--;
+    }
 }
--- test/heap_test.c
+++ test/heap_test.c
@@ -36,10 +36,62 @@ void it_inserts(void **state) {
     assert_int_equal(heap_get_max(&heap), 8);
 }
 
+void it_deletes(void **state) {
+    struct MaxHeap heap = ((struct teststate_t *)(*state))->heap;
+    test_data(&heap);
+    assert_int_equal(heap_get_max(&heap), 8);
+    heap_remove(&heap, 0);
+    assert_int_equal(heap_get_max(&heap), 4);
+    heap_remove(&heap, 0);
+    assert_int_equal(heap_get_max(&heap), 3);
+}
+
+void it_gets_size(void **state) {
+    struct MaxHeap heap = ((struct teststate_t *)(*state))->heap;
+    test_data(&heap);
+    assert_int_equal(get_size(&heap), 4);
+}
+
+void it_is_empty(void **state) {
+    struct MaxHeap heap = ((struct teststate_t *)(*state))->heap;
+    assert_int_equal(heap_is_empty(&heap), true);
+}
+
+void it_extracts_max(void **state) {
+    struct MaxHeap heap = ((struct teststate_t *)(*state))->heap;
+    test_data(&heap);
+    int max = extract_max(&heap);
+    assert_int_equal(max, 8);
+    assert_int_not_equal(heap_get_max(&heap), 8);
+}
+
+/* void it_heapifies_array(void **state) {
+    struct MaxHeap heap = ((struct teststate_t *)(*state))->heap;
+    test_data(&heap);
+    int max = extract_max(&heap);
+    assert_int_equal(max, 8);
+    assert_int_not_equal(heap_get_max(&heap), 8);
+} */
+
+void it_performs_heapsort(void **state) {
+    int arr[] = {23, 3, 42, 18, 7, 21};
+    heapsort(arr, 6);
+    assert_int_equal(arr[0], 3);
+    assert_int_equal(arr[5], 42);
+}
+
 int main(void) {
     printf("Starting heap tests...\n");
     const struct CMUnitTest tests[] = {
         cmocka_unit_test_setup_teardown(it_inserts, test_setup, test_teardown),
+        cmocka_unit_test_setup_teardown(it_deletes, test_setup, test_teardown),
+        cmocka_unit_test_setup_teardown(it_gets_size, test_setup,
+                                        test_teardown),
+        cmocka_unit_test_setup_teardown(it_is_empty, test_setup, test_teardown),
+        cmocka_unit_test_setup_teardown(it_extracts_max, test_setup,
+                                        test_teardown),
+        cmocka_unit_test_setup_teardown(it_performs_heapsort, test_setup,
+                                        test_teardown),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
